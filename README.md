e9f8a6a47733ecd6f99837d3c19b55b343140752
Lothar Rubusch <l.rubusch@gmail.com>
2021-03-28 17:26:03 +0200

    d-compose: ongoing
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- README.md
+++ README.md
@@ -4,6 +4,16 @@
 A docker image for patch development. A ``build.sh`` script will in a second step clone a staging tree. ``configs`` and ``staging`` tree will be setup outside the container, but shared with the container.  
 
 
+## Tools Needed
+
+```
+$ sudo curl -L "https://github.com/docker/compose/releases/download/1.28.6/docker-compose-$(uname -s)-$(uname -m)" -o /usr/local/bin/docker-compose
+$ sudo chmod a+x /usr/local/bin/docker-compose
+```
+
+NB: Where 1.28.6 is the latest version (currently not supported by devian/ubuntu package management)  
+
+
 ## Build
 
 The setup needs a gmail email address for patch delivery via ``git send-email``. Many other email providers are possible in general, too  
--- docker/build_context/build.sh
+++ docker/build_context/build.sh
@@ -1,36 +1,48 @@
 #!/bin/bash -e
 HOME="/home/$(whoami)"
 BRANCH="master"
-SOURCES="${HOME}/u-boot"
-CONFIGS="${HOME}/configs"
-SHARES=( ${SOURCES} ${CONFIGS} )
+SOURCES_DIR="${HOME}/u-boot"
+CONFIGS_DIR="${HOME}/configs"
+SECRETS_DIR="${HOME}/secrets"
+SHARES=( ${SOURCES_DIR} ${CONFIGS_DIR} ${SECRETS_DIR} )
 
 ## fix permissions
 for item in ${SHARES[*]}; do
     test -e ${item} && sudo chown $(whoami).$(whoami) -R ${item}
 done
 
+if [[ ! -f ${SECRETS_DIR}/.gitconfig ]]; then
+    cp ${CONFIGS_DIR}/.gitconfig ${SECRETS_DIR}/.gitconfig
+    echo "!!! FIRST TIME INSTALLATION !!!"
+    echo "!!!"
+    echo "!!! PLEASE FILL SECRET INFORMATION (NOT GIT TRACKED) IN"
+    echo "!!! '${SECRETS_DIR}/.gitconfig'"
+    echo "!!! THEN LOGIN AGAIN"
+    echo "!!!"
+    exit 0
+fi
+
 cd ${HOME}
 
 ## get sources
-if [[ -d "${SOURCES}/.git" ]]; then
-  cd ${SOURCES}
+if [[ -d "${SOURCES_DIR}/.git" ]]; then
+  cd ${SOURCES_DIR}
   git fetch --all
 else
   git clone -j4 --branch $BRANCH https://gitlab.denx.de/u-boot/u-boot.git u-boot
 fi
 
 ## generate TAGS
-cd ${SOURCES}
+cd ${SOURCES_DIR}
 rm -f ./TAGS
 find . -regex ".*\.\(h\|c\)$" -exec etags -a {} \;
 
 ## setup checkpatch / codespell
-echo "#!/bin/sh" > ${SOURCES}/.git/hooks/post-commit
-echo "exec git show --format=email HEAD | ./scripts/checkpatch.pl --strict --codespell" >> ${SOURCES}/.git/hooks/post-commit
-chmod a+x ${SOURCES}/.git/hooks/post-commit
+echo "#!/bin/sh" > ${SOURCES_DIR}/.git/hooks/post-commit
+echo "exec git show --format=email HEAD | ./scripts/checkpatch.pl --strict --codespell" >> ${SOURCES_DIR}/.git/hooks/post-commit
+chmod a+x ${SOURCES_DIR}/.git/hooks/post-commit
 
-cd ${SOURCES}
+cd ${SOURCES_DIR}
 make clean
 
 echo "READY."
