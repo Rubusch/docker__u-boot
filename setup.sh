c079558f38ef6c014ef1cb4051c6591435b6dd8b
Lothar Rubusch <l.rubusch@gmail.com>
2021-10-28 21:12:58 +0200

    base image: bump version, fix setup.sh
    Signed-off-by: Lothar Rubusch <l.rubusch@gmail.com>

--- setup.sh
+++ setup.sh
@@ -1,15 +1,16 @@
 #!/bin/sh -e
 build()
 {
-	pushd "${1}" &> /dev/null
+	CONTAINER_NAME="$(grep "container_name:" -r "${1}/docker-compose.yml" | awk -F: '{ print $2 }' | tr -d ' ')"
+	cd "${1}"
 	echo "UID=$(id -u)" > .env
 	echo "GID=$(id -g)" >> .env
 	if [ -n "${2}" ]; then
 	    docker-compose build
 	else
-	    docker-compose up
+	    docker-compose up --exit-code-from "${CONTAINER_NAME}"
 	fi
-	popd &> /dev/null
+	cd -
 }
 
 DRYRUN="${1}"
@@ -18,18 +19,16 @@ DRYRUN="${1}"
 IMAGE="sandbox"
 test -z "${DOCKERDIR}" && DOCKERDIR="docker"
 
-TAG="$(grep "ENV DOCKER_BASE_TAG" -r "./${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }')"
-TAG="${TAG//\"}"
+TAG="$(grep "ARG DOCKER_BASE_TAG" -r "${DOCKERDIR}/build_context/Dockerfile" | awk -F= '{ print $2 }' | tr -d '"')"
 
-CONTAINER="$(docker images | grep "/${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
+CONTAINER="$(docker images | grep "${IMAGE}" | grep "${TAG}" | awk '{print $3}')"
 
 if [ -z "${CONTAINER}" ]; then
 	git clone "https://github.com/Rubusch/docker__${IMAGE}.git" "${IMAGE}"
-	build "./${IMAGE}" "${DRYRUN}"
+	build "./${IMAGE}/docker" "${DRYRUN}"
 fi
 
 ## docker container
 build "./${DOCKERDIR}" "${DRYRUN}"
 
 echo "READY."
-
